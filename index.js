13aa7fcbd1c2216a71457a2bdb2456e4a15f518b
Juana Villalpando <juana.v14@gmail.com>
2019-12-16 14:17:48 -0800

    complete 2 challenges

--- index.js
+++ index.js
@@ -39,10 +39,15 @@ function addNumbers(num1, num2) {
  *   email: "leia@leia.com",
  * }
 */
-function makePersonObject(/* code here */) {
-  
+function makePersonObject(id, name, email) {
+  const PersonObject = {
+    id: id,
+    name:name,
+    email: email
+  };
+  return PersonObject;
 }
-
+// console.log(makePersonObject(5, Leia, leia@leia.com));
 /**
  * ### Challenge `getName`
  * 
@@ -56,8 +61,8 @@ function makePersonObject(/* code here */) {
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
-  /* code here */
+function getName(FullName) {
+  return "Hello, my name is " + FullName.name;
 }
 
 /**
@@ -73,8 +78,9 @@ function getName(/* code here */) {
  *         and returns a string like `Hello, my name is {name}`.
  *         where `{name}` is the name passed into `makeSmartPerson`.
 */
-function makeSmartPerson(/* code here */) {
-  /* code here */
+function makeSmartPerson(Person) {
+
+  return "Hello, my name is " +  Person.name;
 }
 
 
